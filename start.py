6988bd72f713254150aa2b3324bde95b8b5eacb0
ApaTia Python <108146296+ApaT1aPY@users.noreply.github.com>
2023-03-03 08:49:23 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,29 @@
+import random
+import eel
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from config import token
+
+
+@eel.expose
+def add_token(tken):
+    my_file = open("config.py", "w+")
+    my_file.write(f"token='{tken}'")
+    my_file.close()
+
+
+session = vk_api.VkApi(token=token)
+
+
+
+@eel.expose
+def send_message(user_id, message):
+    session.method("messages.send", {
+                   "user_id": user_id,
+                   "message": message,
+                   "random_id": 0}) # randrange(100000))
+    print("Сообщение отправлено")
+
+
+eel.init("interface")
+eel.start("main.html", size=(425, 700))
